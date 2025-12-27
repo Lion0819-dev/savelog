b929114d06f4d094196e358dee2cba9ebddd9c05
Lion <strongking0819@gmail.com>
2025-12-27 13:54:21 +0900

    貯金追加の入力画面を作成

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="SaveLog")
 
+templates = Jinja2Templates(directory="app/templates")
 
-@app.get("/")
-def root():
-    return {"app": "SaveLog", "status": "ok"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 
+@app.get("/add", response_class=HTMLResponse)
+def add_form(request: Request):
+    return templates.TemplateResponse(
+        "add.html",
+        {"request": request}
+    )
